ba597aa1867bddcbcaff939a6c73fc337264c8ec
rcywongaa <rcywongaa@gmail.com>
2014-04-17 20:19:50 +0800

    Bluetooth server setup
    Added some codes from
    
    http://developer.android.com/guide/topics/connectivity/bluetooth.html#SettingUp
    related to setting up the bluetooth server
    
    Also added some TODOs

--- src/ece/course/extkeytab/MainActivity.java
+++ src/ece/course/extkeytab/MainActivity.java
@@ -1,22 +1,79 @@
 package ece.course.extkeytab;
 
+import java.io.IOException;
+import java.util.UUID;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	final String NAME = "ExtKeyTab";
+	final UUID MY_UUID = UUID.randomUUID();
+	BluetoothAdapter mAdapter;
 
-	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		//TODO: Code for creating the EditText layout
+		
+		mAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (!mAdapter.isEnabled()) {
+		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		    startActivityForResult(enableBtIntent, 1);
+		}
+		
+		Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+		discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 0);
+		startActivity(discoverableIntent);
+		
+		new Thread(new AcceptThread()).start();
+		
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	
+	private class AcceptThread extends Thread {
+	    private final BluetoothServerSocket mmServerSocket;
+	 
+	    public AcceptThread() {
+	        // Use a temporary object that is later assigned to mmServerSocket,
+	        // because mmServerSocket is final
+	        BluetoothServerSocket tmp = null;
+	        try {
+	            // MY_UUID is the app's UUID string, also used by the client code
+	            tmp = mAdapter.listenUsingRfcommWithServiceRecord(NAME, MY_UUID);
+	        } catch (IOException e) { }
+	        mmServerSocket = tmp;
+	    }
+	 
+	    public void run() {
+	        BluetoothSocket socket = null;
+	        // Keep listening until exception occurs or a socket is returned
+	        while (true) {
+	            try {
+	                socket = mmServerSocket.accept();
+	            } catch (IOException e) {
+	                break;
+	            }
+	            // If a connection was accepted
+	            if (socket != null) {
+	            	try {
+						mmServerSocket.close();
+					} catch (IOException e) { }
+	            	break;
+	            }
+	        }
+	        //TODO: Code for handling data flow after receiving BluetoothSocket socket
+	    }
+	 
+	    /** Will cancel the listening socket, and cause the thread to finish */
+	    public void cancel() {
+	        try {
+	            mmServerSocket.close();
+	        } catch (IOException e) { }
+	    }
 	}
-
 }
